903303f8f42be4910584dfc8ddc8e80ae7bacee0
WantianZhao <wantian2@illinois.edu>
2020-03-02 00:13:47 -0600

    Mar 2nd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,23 +4,22 @@ import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 
 fun hello(): String {
-    return "Hello, world!"
+    return "Hello World!"
 }
 
-data class CalculatorRequest(
-    val operation: String, val first: Int, val second: Int
-)
-data class Result(val first: Int, val second: Int, val result: Int)
-
+data class CalculatorRequest(val operation: String, val first: Int, val second: Int)
+data class Result(val operation: String, val first: Int, val second: Int, val result: Int)
 
 fun Application.adder() {
     val counts: MutableMap<String, Int> = mutableMapOf()
@@ -28,14 +27,31 @@ fun Application.adder() {
         gson { }
     }
     routing {
-        get("/count/{first}") {
-            val firstCount = counts.getOrDefault(call.parameters["first"],0) + 1
-            counts[call.parameters["first"].toString()] = firstCount
-            call.respondText(firstCount.toString())
-        }
-
         get("/") {
             call.respondText(hello())
+        }
+        get("/count/{first}") {
+            val firstCount = counts.getOrDefault(call.parameters["first"], 0) + 1
+            counts[call.parameters["first"].toString()] = firstCount
+            println(call.parameters["first"] + ": $firstCount")
+            call.respondText(firstCount.toString())
+        }
+        post("/calculate") {
+            try {
+                val request = call.receive<CalculatorRequest>()
+                val result = when (request.operation) {
+                    "add" -> request.first + request.second
+                    "subtract" -> request.first - request.second
+                    "multiply" -> request.first * request.second
+                    "divide" -> request.first / request.second
+                    else -> throw Exception("${request.operation} is not supported")
+                }
+                var response = Result(request.operation, request.first, request.second, result)
+                call.respond(response)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+
         }
         get("/{operation}/{first}/{second}") {
             try {
@@ -43,44 +59,20 @@ fun Application.adder() {
                 val first = call.parameters["first"]!!.toInt()
                 val second = call.parameters["second"]!!.toInt()
                 val result = when (operation) {
-                    "add" -> request.first + request.second
-                    "substract" -> first - second
+                    "add" -> first + second
+                    "subtract" -> first - second
                     "multiply" -> first * second
-                    else -> Exception("${request.operation} is not supported")
+                    "divide" -> first / second
+                    else -> throw Exception("$operation is not supported")
                 }
-
-                val addResult = Result(first, second, result as Int)
-                call.respond(addResult)
+                val operationResult = Result(operation, first, second, result)
+                call.respond(operationResult)
             } catch (e: Exception) {
-                println(e)
-                call.respond(HttpStatusCode.BadRequest)
+                call.respond(HttpStatusCode.BadRequest, e)
             }
-
         }
-
-    }
-    post("/calculate") {
-        try {
-            val request = call.receive<CalculatorRequest>()
-            val result = when (request.operation) {
-                "add" -> request.first + request.second
-                else -> throw java.lang.Exception("${request.operation} is not supported")
-            }
-
-            val response = Result(request.operation, request.first, request.second, result)
-            call.respond(response)
-
-
-        } catch (e:Exception) {
-            call.respond(HttpStatusCode.BadRequest)
-        }
-
     }
 }
-
 fun main() {
-    embeddedServer(Netty, port = 8080, module = Application::adder).start(wait = true)
-
+    embeddedServer(Netty, 8080, module = Application::adder).start(wait = true)
 }
-
-
--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -1,57 +1,44 @@
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import io.ktor.application.Application
-import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
-import io.netty.handler.codec.http.HttpHeaders.addHeader
+import io.ktor.http.HttpMethod
+import io.ktor.server.testing.setBody
 
 class TestMain : StringSpec({
     "should retrieve root path properly" {
-    withTestApplication(Application::adder) {
-        handleRequest(HttpMethod.Get, "/").apply {
-
-            response.status() shouldBe HttpStatusCode.OK
-            response.content shouldBe "Hello,world"
-        }
-    }
-    }
-
-
-    "should count routes path properly" {
         withTestApplication(Application::adder) {
-            handleRequest(HttpMethod.Get, "/count/first").apply {
+            handleRequest(HttpMethod.Get, "/").apply {
                 response.status() shouldBe HttpStatusCode.OK
-                response.content shouldBe "1"
-            }
-            handleRequest(HttpMethod.Get, "/count/first").apply {
-                response.status() shouldBe HttpStatusCode.OK
-                response.content shouldBe "2"
+                response.content shouldBe "Hello World!"
             }
         }
     }
-    "should calculate routes path properly" {
+    "should accept post calculate request" {
         withTestApplication(Application::adder) {
-            handleRequest(HttpMethod.Get, "/calculate") {
-                addHeader("content-type","application/json")
-                setBody(""""
+            handleRequest(HttpMethod.Post, "/calculate") {
+                addHeader("content-type", "application/json")
+                setBody("""
 {
-    "operation": "add"
-    "first": 4
-    "second": 3
-}
-                """".trim())
-                response.status() shouldBe HttpStatusCode.OK
-                response.content shouldBe "1"
-            }.apply
+  "operation": "add",
+  "first": 4,
+  "second": 5
+}                   
+                """.trim())
+            }.apply {
                 println(response.content)
-                response.status shouldBe HttpStatusCode
+                response.status() shouldBe HttpStatusCode.OK
+            }
         }
     }
-
-
-
-
+    "should count routes properly" {
+        withTestApplication(Application::adder) {
+            handleRequest(HttpMethod.Get, "/count/first").apply {
+                response.status() shouldBe HttpStatusCode.OK
+                response.content shouldBe "1"
+            }
+        }
+    }
 })
